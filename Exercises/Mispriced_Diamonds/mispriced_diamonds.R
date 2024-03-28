22f31315d1f408ef8a218c12ef2ebd475528fa65
Patrick_H <patrick.c.hennessey@gmail.com>
2024-03-27 23:12:51 -0400

    Added dataset and v1 script

---
+++ Exercises/Mispriced_Diamonds/mispriced_diamonds.R
@@ -0,0 +1,7 @@
+mydata <- read.csv("C:/Users/Admin/GitHub_Cloned_Repositories/R/Exercises/Datasets/P2-Mispriced-Diamonds.csv")
+
+ggplot(data=mydata, 
+       aes(x=carat, y=price)) + 
+  geom_point()
+
+  
