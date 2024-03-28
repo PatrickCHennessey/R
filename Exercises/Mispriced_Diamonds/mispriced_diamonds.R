a49001255af7f99eec5cbeee49f11bfa25e96a32
Patrick_H <patrick.c.hennessey@gmail.com>
2024-03-27 23:20:33 -0400

    Added v3 of R script and resulting image

--- Exercises/Mispriced_Diamonds/mispriced_diamonds.R
+++ Exercises/Mispriced_Diamonds/mispriced_diamonds.R
@@ -1,7 +1,7 @@
 mydata <- read.csv("C:/Users/Admin/GitHub_Cloned_Repositories/R/Exercises/Datasets/P2-Mispriced-Diamonds.csv")
 
-ggplot(data=mydata, 
+ggplot(data=mydata[mydata$carat<2.5,], 
        aes(x=carat, y=price, colour = clarity)) + 
-  geom_point()
-
+  geom_point(alpha=0.1) +
+  geom_smooth()
   
