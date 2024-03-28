e9522a0b560925670c09228591d74e68f395d60c
Patrick_H <patrick.c.hennessey@gmail.com>
2024-03-28 16:51:14 -0400

    Finished using_variables.R script, added image

---
+++ Exercises/Sandbox/using_variables.R
@@ -0,0 +1,26 @@
+# Define values for "A" and "B" variables
+A <- 10
+B <- 5
+
+# Combine values of "A" and "B" variables into new variable "C"
+C <- A+B
+
+# Define variable var1
+var1 <- 2.5
+
+# Define variable var2
+var2 <- 4
+
+# Divide one variable by another variable
+result <- var1 / var2
+result
+
+# Find square root of a variable
+answer <- sqrt(var2)
+answer
+
+# Print a message 
+greeting <- "Hello"
+name <- "Bob"
+message <- paste(greeting, name)
+message
