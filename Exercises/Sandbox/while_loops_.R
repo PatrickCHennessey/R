4030d04b7d944006734ebeddedc8fc00401de398
Patrick_H <patrick.c.hennessey@gmail.com>
2024-04-02 15:34:42 -0400

    Test of renaming file

---
+++ Exercises/Sandbox/while_loops_.R
@@ -0,0 +1,34 @@
+# # setwd("Directory of desired PATH")
+# 
+# # While Loop
+# 
+# while(TRUE){
+#   print("Hello")
+# }
+# 
+# counter <- 1
+# while (counter < 12){
+#   print(counter)
+#   counter <- counter + 1
+# }
+
+# # Experiment with Console
+# 
+# x <- 5
+# x 
+# y <- 4
+# y
+
+# # For Loop
+# 
+# for(i in 1:5){
+#   print("Hello")
+# }
+# 
+# for(i in 5:10){
+#   print("Hello")
+# }
+# 
+# for(i in 1:10){
+#   print(i)
+# }
